ba9ceac7f650dc4b1f2fcb1619579236662a3c9d
Agon Bina <agon_bina@hotmail.com>
2015-03-11 11:13:44 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+
+### Install
+```
+npm install -g vue-cli
+```
+
+### Create a new component
+```
+vue component [componentName|String]
+```
---
+++ index.js
@@ -0,0 +1,8 @@
+var ronin = require('ronin');
+
+var program = ronin({
+  path: __dirname,
+  desc: 'Program description'
+});
+
+program.run();
