35435c130ac4f214c34dde6947456f9202ff9b95
Agon Bina <agon_bina@hotmail.com>
2015-03-13 12:37:29 +0100

    [feature] middleware to add a git origin repo based on the global config settings

---
+++ middleware/add-remote.js
@@ -0,0 +1,24 @@
+
+var path = require('path')
+var git = require('gulp-git')
+var config = require('rc')('vue').git
+
+module.exports = function gitInit (next) {
+  var name = this.options['_'][0]
+  var repo = ''
+
+  if(!config) next()
+
+  repo += config.host === 'gitlab'
+            ? 'git@gitlab.com'
+                : config.host === 'bitbucket'
+                  ? 'git@bitbucket.org'
+                  : 'git@github.com'
+
+  repo +=  config.organization + '/' + name + '.git'
+
+  git.addRemote('origin', repo, { cwd: path.join(process.cwd(), name) }, function (err) {
+    if(err) throw new Error(err.message)
+    next()
+  })
+}
